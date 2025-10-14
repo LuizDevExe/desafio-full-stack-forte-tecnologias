d9aa273088bc80eb6f1de2b7864bf62be596179a
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-10-14 19:32:54 -0300

    feat: Adiciona mensagens validação de tipos ao createCompanyDto

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,11 +1,11 @@
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateCompanyDto {
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({message: 'Campo não pode estar vazio'})
+  @IsString({message: 'Campo precisa ser um texto válido'})
   name: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({message: 'Campo não pode estar vazio'})
   @Length(14, 14, { message: 'O cnpj deve conter 14 caracteres' })
   cnpj: string;
 }
