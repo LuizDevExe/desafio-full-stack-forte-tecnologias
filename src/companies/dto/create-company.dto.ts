ec04ea60e97865854b4d4a110fe1b8214b7ab6bc
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-10-13 21:23:30 -0300

    feat: Adiciona validação aos campos do create-company

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCompanyDto {}
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateCompanyDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @Length(14, 14, { message: 'O cnpj deve conter 14 caracteres' })
+  cnpj: string;
+}
