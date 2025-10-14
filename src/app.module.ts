9bebd44c695f6ff34b257f400e4c07e4184aeb13
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-10-14 19:32:34 -0300

    chore: Retira arquivos não utilizados e suas importações no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { CompaniesModule } from './companies/companies.module';
 import { EmployeesModule } from './employees/employees.module';
 import { AssetsModule } from './assets/assets.module';
 
+
+
 @Module({
   imports: [CompaniesModule, EmployeesModule, AssetsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
